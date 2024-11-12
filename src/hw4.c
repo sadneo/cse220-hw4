5924acd0c4f20b5a0ea6175bd7b8ef141d9592e7
sadneo <haywardeel@gmail.com>
2024-11-12 08:33:58 -0500

    feat: added init

--- src/hw4.c
+++ src/hw4.c
@@ -62,6 +62,17 @@ void read_to_buffer(char[] buffer, int conn_fd) {
     }
 }
 
+char[5] SEND_ERROR_BUFFER = "";
+void send_error(int conn_fd, int message) {
+    // error is always 5 characters long
+    sprintf(&SEND_ERROR_BUFFER, "E %d", message);
+    send(conn_fd, SEND_ERROR_BUFFER, 5, 0);
+}
+
+void send_a(int conn_fd, char* message, int len) {
+    send(conn_fd, message, len, 0);
+}
+
 // EXPECTED:
 // begin: B 11 11
 // shoot: S 1 1
@@ -81,28 +92,83 @@ int main() {
     int conn_fd2 = bind_socket(listen_fd2, PORT2);
     printf("sockets create/bind/listen/accept done\n");
 
-    char buffer[BUFFER_SIZE] = {0};
-    read_to_buffer(buffer, conn_fd1);
-
     // 0 for blank tiles
     // 12345 for ships, positive if player1, negative if player2
     int width, int height;
     int *board = NULL;
-    if (strcmp(buffer, "B") != 0) {
-        error();
-        goto cleanup;
+    char buffer[BUFFER_SIZE] = {0};
+
+    // begin
+    while (1) {
+        read_to_buffer(buffer, conn_fd1);
+        if (strcmp(buffer, "B") != 0) {
+            send_error(conn_fd1, 100)); // Invalid packet type (Expected Begin packet)
+            continue;
+        }
+        int scanned = sscanf(buffer, "%c %d %d", &packet_type, &width, &height);
+        if (sscanf(buffer, "B %d %d", &width, &height) != 2) {
+            send_error(conn_fd1, 200)); // Invalid Begin packet (invalid number of parameters)
+            continue;
+        }
+        if (width < 10 || height < 10) {
+            // 200: Invalid Begin packet (invalid number of parameters)
+            // width or height less than 10, this is best matching error
+            send_error(conn_fd1, 200));
+            continue;
+        }
+        board = malloc(sizeof(int) * width * height);
+        send_a(conn_fd1, "A", 1);
+        break;
     }
 
-    sscanf(buffer, "B %d %d", &width, &height);
-    if (width < 10 || height < 10) {
-        printf("width or height less than 10");
+    while (1) {
+        read_to_buffer(buffer, conn_fd2);
+        if (strcmp(buffer, "B") != 0) {
+            send_error(conn_fd1, 100)); // Invalid packet type (Expected Begin packet)
+            continue;
+        }
+        break;
     }
-    board = malloc(sizeof(int) * width * height);
 
-    //     The server should attempt to bind/listen/accept on both ports. (2201 is player 1 and 2202 is player 2)
-    // The server should expect a Begin packet from player 1 that contains a board size of at least 10 x 10.
+    while (1) {
+        read_to_buffer(buffer, conn_fd1);
+        if (strcmp(buffer, "I") != 0) {
+            send_error(conn_fd1, 101)); // Invalid packet type (Expected Init packet)
+            continue;
+        }
+
+        char *buffer_r = buffer + 2; // skip "I "
+        for (int i = 0; i < 5; i++) {
+            int type, rot, col, row;
+            if (sscanf(buffer, "%d %d %d %d", &type, &rot, &col, &row) != 4) {
+                send_error(conn_fd1, 201)); // Invalid Init packet (invalid number of parameters)
+                continue;
+            }
+            if (row < 0 || col < 0 || col >= width || row >= height) {
+                // 300: Invalid Initialize packet (shape out of range)
+                send_error(conn_fd1, 300);
+                continue;
+            }
+
+            int location = board + row * width + col;
+
+            // attempt to place pieces
+            // 301: Invalid Initialize packet (rotation out of range)
+            // 302: Invalid Initialize packet (ship doesn’t fit in game board)
+            // 303: Invalid Initialize packet (ships overlap)
+        }
+
+        send_a(conn_fd1, "A", 1);
+        break;
+    }
+
+
+    //      The server should attempt to bind/listen/accept on both ports. (2201 is player 1 and 2202 is player 2)
+    //      The server should expect a Begin packet from player 1 that contains a board size of at least 10 x 10.
     // Acknowledge or send errors until a Begin is received from both player 1
-    //  The server should expect a Begin packet from player 2 that contains no parameters
+    //
+    // The server should expect a Begin packet from player 2 that contains no parameters
+    //
     // The server should expect a Initialize packet on each port (first player 1, then player 2)
     // Acknowledge or send errors until a valid Initialization is received from each player
     // The server should expect either a Shoot, Query, or Forfeit packet from player 1.
@@ -114,22 +180,6 @@ int main() {
     // Same behavior as above
     // If a shoot packet sinks the last ship, send a Halt to both players
 
-
-
-    /*{
-        printf("[Server] Enter message: ");
-        memset(buffer, 0, BUFFER_SIZE);
-        fgets(buffer, BUFFER_SIZE, stdin);
-        buffer[strlen(buffer)-1] = '\0';
-        if (strcmp(buffer, "quit") == 0) {
-            printf("[Server] Quitting...\n");
-            send(conn_fd, buffer, strlen(buffer), 0);
-            break;
-        }
-        send(conn_fd, buffer, strlen(buffer), 0);
-    }*/
-
-cleanup:
     close(conn_fd1);
     close(conn_fd2);
     close(listen_fd1);
